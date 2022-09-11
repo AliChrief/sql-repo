584fb3cee598b2d80cc3f82c22f727cca29953d8
ALichrieff <alichrief12gmail.com>
2022-09-11 22:51:25 +0300

    edited

--- bestmovie.sql
+++ bestmovie.sql
@@ -1,3 +1,3 @@
 SELECT name,earnings_rank
-FROM movie,oscar
-WHERE oscar.type= "Best-Picture" AND movie.id=oscar.movie_id 
+FROM Movie,Oscar
+WHERE Oscar.type= "Best-Picture" AND Movie.id=Oscar.movie_id 
